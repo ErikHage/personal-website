abb5e778f87940c7980688ef98c0e3c9ff7ed753
Erik Hage <ehage4@gmail.com>
2023-04-29 02:27:53 -0400

    Added professional experience, personal projects, and rw&b pages

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,15 @@
 import Router, { Express, Request, Response } from 'express';
-
-import constants from './helpers/constants';
 import path from 'path';
 
+import constants from './helpers/constants';
+
 const router: Express = Router();
+
+const defaultNavOptions = {
+  professionalExperienceClasses: '',
+  personalProjectsClasses: '',
+  rwabClasses: '',
+};
 
 const buildRouter = (appRoot: string): Express => {
   router.set('view engine', 'ejs');
@@ -12,7 +18,40 @@ const buildRouter = (appRoot: string): Express => {
   router.get('/', (request: Request, response: Response) => {
     response.render('index', {
       // todo data here, maybe random photo or project highlights
-      constants,
+      urls: constants.urls,
+      nav: {
+        ...defaultNavOptions,
+      },
+    });
+  });
+
+  router.get('/professional-experience', (request: Request, response: Response) => {
+    response.render('professional-experience', {
+      urls: constants.urls,
+      nav: {
+        ...defaultNavOptions,
+        professionalExperienceClasses: 'active',
+      },
+    });
+  });
+
+  router.get('/personal-projects', (request: Request, response: Response) => {
+    response.render('personal-projects', {
+      urls: constants.urls,
+      nav: {
+        ...defaultNavOptions,
+        personalProjectsClasses: 'active',
+      },
+    });
+  });
+
+  router.get('/red-white-and-brew', (request: Request, response: Response) => {
+    response.render('red-white-and-brew', {
+      urls: constants.urls,
+      nav: {
+        ...defaultNavOptions,
+        rwabClasses: 'active',
+      },
     });
   });
 
