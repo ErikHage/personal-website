c3200e15b667f94f397aaa1887d8efeaa0436bb1
Erik Hage <ehage4@gmail.com>
2023-04-30 00:28:20 -0400

    added more prof experience

--- src/data/professional-experience/jobs/automatic-data-processing.ts
+++ src/data/professional-experience/jobs/automatic-data-processing.ts
@@ -12,12 +12,57 @@ const data: WorkData = {
       tenure: 'October 2017 - September 2019',
     },
   ],
-  summary: 'A member of the Identity Management team responsible for security of user accounts, authentication, federation, and session resolution for a large metadata driven human resources platform.',
-  projects: [ 
+  summary: 'As a member of the Identity Management team, I was responsible for security of user accounts, authentication, federation, and session resolution for a large metadata driven human resources platform.',
+  projects: [
     {
-      title: 'todo',
-      summary: 'todo',
-      skillsUsed: [ 'todo', 'todo', ],
+      title: 'Impersonation',
+      summary: 'Implemented impersonation functionality, allowing support team members to act as specific users, which led to a large reduction in issue resolution time and faster bug fix turnaround.',
+      skillsUsed: [ 'NodeJS', 'MySQL', 'DynamoDB', ],
+    },
+    {
+      title: 'Multi-Factor Authentication',
+      summary: 'Developed email, SMS, and voice call mechanisms to step up users to a higher level of assurance. Implemented access control based on level of assurance, providing higher security for sensitive data.',
+      skillsUsed: [ 'NodeJS', 'MySQL', 'DynamoDB', 'AWS', ],
+    },
+    {
+      title: 'Policy/Rules Engine',
+      summary: 'Designed and implemented a rules engine that applied policies based on client configuration, allowing clients to differentiate authentication methods across user groups.',
+      skillsUsed: [ 'NodeJS', 'MySQL', ],
+    },
+    {
+      title: 'GDPR',
+      summary: 'Allow European Union users to request that their data be removed from the system and provide support functionality to perform those actions.',
+      skillsUsed: [ 'NodeJS', 'MySQL', 'DynamoDB', ],
+    },
+    {
+      title: 'Refresh Tokens',
+      summary: 'Implemented the ability for long running processes to request refresh tokens that would allow a session refresh if the process ran longer than session expiration.',
+      skillsUsed: [ 'NodeJS', 'MySQL', 'DynamoDB', ],
+    },
+    {
+      title: 'Search Framework with Pagination',
+      summary: 'Developed a search framework that could be configured by database model to paginate records based on specific fields and ordering.',
+      skillsUsed: [ 'NodeJS', 'MySQL', ],
+    },
+    {
+      title: 'Obligations Framework',
+      summary: 'Designed and implemented functionality to lock users out of the system until specific conditions were met. Only pages and apis required to meet the obligation were available to the user.',
+      skillsUsed: [ 'NodeJS', 'MySQL', 'DynamoDB', ],
+    },
+    {
+      title: 'Feature Toggles',
+      summary: 'Designed and implemented a feature toggle system and an admin console to manage configuration, which enabled incremental rollouts and was adopted by multiple teams across the organization, allowing immediate rollbacks of problematic features.',
+      skillsUsed: [ 'NodeJS', 'DynamoDB', ],
+    },
+    {
+      title: 'SAML as a Service Provider',
+      summary: 'Developed a Java service, using Spring Boot, to handle SAML responses, allowing our platform to integrate with legacy and external systems and enabling the sales team to target additional clients.',
+      skillsUsed: [ 'Java', 'Spring Boot', 'NodeJS', 'MySQL', 'DynamoDB', ],
+    },
+    {
+      title: 'SAML as an Identity Provider',
+      summary: 'Expanded on the SAML Java service to handle SAML requests, allowing our platform to become an identity provider for other applications across the company and external clients.',
+      skillsUsed: [ 'Java', 'Spring Boot', 'NodeJS', 'MySQL', 'DynamoDB', ],
     },
   ],
 };
