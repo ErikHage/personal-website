f58e302faaaf99c25e542a5985ebe962299742ce
Erik Hage <ehage4@gmail.com>
2023-09-11 20:31:21 -0400

    Added CLS as a project under my job at LMAX

--- src/data/professional-experience/jobs/lmax-group.ts
+++ src/data/professional-experience/jobs/lmax-group.ts
@@ -11,7 +11,18 @@ const data: WorkData = {
   ],
   summary: 'A global financial technology company which operates multiple institutional execution venues for electronic ' +
     'foreign exchange (FX) and crypto currency trading.',
-  projects: [],
+  projects: [
+    {
+      title: 'Continuous Linked Settlement (CLS) for Currency Instruments',
+      summary: 'CLS is an industry initiative to eliminate the settlement risk in foreign exchange ' +
+        'transactions. By settling trades immediately you reduce risk of to one of the parties ' +
+        'defaulting. This project focused on allowing currency instruments to be marked as eligible ' +
+        'for CLS if both settlement parties had such a relationship in place. In such cases those ' +
+        'trades could be excluded from daily risk calculations, allowing for a trading member to ' +
+        'complete a greater number of trades before hitting their risk limit.',
+      skillsUsed: [ 'VueJS', 'Java', ],
+    },
+  ],
 };
 
 export default data;
