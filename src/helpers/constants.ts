6d59d5b08a80459a95d61a386b4b47fb9e0d9a70
Erik Hage <ehage4@gmail.com>
2023-09-11 20:14:59 -0400

    Add Tools/Apps to nav as dropdown, include Recipes App

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -2,5 +2,6 @@ export default {
   urls: {
     GITHUB: 'https://github.com/ErikHage',
     LINKED_IN: 'https://www.linkedin.com/in/erik-hage-9553a642/',
+    RECIPE_APP: 'https://theferalrooster.com/recipes',
   },
 };
