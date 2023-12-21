ded37c0f1481ea94d61d77a040aaf6a2addcd92c
Erik Hage <ehage4@gmail.com>
2023-12-20 22:23:05 -0500

    Added performance work to lmax job details

--- src/data/professional-experience/jobs/lmax-group.ts
+++ src/data/professional-experience/jobs/lmax-group.ts
@@ -20,6 +20,15 @@ const data: WorkData = {
         'for CLS if both settlement parties had such a relationship in place. In such cases those ' +
         'trades could be excluded from daily risk calculations, allowing for a trading member to ' +
         'complete a greater number of trades before hitting their risk limit.',
+      skillsUsed: [ 'VueJS', 'Java', 'MySQL', ],
+    },
+    {
+      title: 'Market Data Feed Performance Testing',
+      summary: 'Increased the performance of market data. Found bottlenecks using profiling and ' +
+        'benchmarking of the mark data feed. Used a probe to stream FIX messages to a separate ' +
+        'process and used histograms to calculate inter-message timing statistics. Added these ' +
+        'latency results to a performance tracker UI to have a real-time view of the effects of ' +
+        'code changes on the latency.',
       skillsUsed: [ 'VueJS', 'Java', ],
     },
   ],
