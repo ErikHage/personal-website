481255f6545bc6d8d38d6b8ed6560f06ddf3e706
Erik Hage <ehage4@gmail.com>
2024-11-22 23:53:11 -0500

    update hobbies

--- src/services/about-me/data/hobbies/hobby-data/gardening.ts
+++ src/services/about-me/data/hobbies/hobby-data/gardening.ts
@@ -4,8 +4,8 @@ const data: Hobby = {
   name: 'Gardening',
   description: 'I\'m a big fan of self-sufficiency, and growing my own fruits and veggies ' +
     'gives me a feeling of accomplishment. I had a large garden when I was younger, but had ' +
-    'to give that up while living in apartments. Now that I own my own home, I\'m building ' +
-    'a new garden.',
+    'to give that up while living in apartments. Now that I own my own home, I\'ve built ' +
+    'a new garden, and planning to expand it for next year.',
   links: [],
   images: [
     {
