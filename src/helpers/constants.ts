e6af3fbf8ade643690b3d8d8fadfd470536ae250
Erik Hage <ehage4@gmail.com>
2025-05-19 01:02:01 -0400

    point to new voracious-appetite app

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -3,7 +3,7 @@ import { NavOptions } from '../handlers/types';
 const urls = {
   GITHUB: 'https://github.com/ErikHage',
   LINKED_IN: 'https://www.linkedin.com/in/erik-hage-9553a642/',
-  VORACIOUS_APPETITE: 'https://theferalrooster.com/recipes',
+  VORACIOUS_APPETITE: 'https://theferalrooster.com/apps/voracious-appetite',
   TIC_TAC_TOE_APP: 'https://theferalrooster.com/apps/tic-tac-toe',
   FERAL_AUTHENTICATION: 'https://theferalrooster.com/auth',
   VIGILANT_HARVEST: 'https://theferalrooster.com/apps/vigilant-harvest',
